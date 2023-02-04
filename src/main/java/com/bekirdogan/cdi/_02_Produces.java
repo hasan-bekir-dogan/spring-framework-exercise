4c7bba621686505de6fcd6d169804167f3856ff4
hasanbekir97 <hasan.bekir.dogan@hotmail.com>
2023-02-04 23:17:45 +0300

    @Produces

--- src/main/java/com/bekirdogan/cdi/_02_Produces.java
+++ src/main/java/com/bekirdogan/cdi/_02_Produces.java
@@ -1,5 +1,23 @@
 package com.bekirdogan.cdi;
 
-@N
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "producetuto")
+@ApplicationScoped
 public class _02_Produces {
+    @Produces   // üretiyor
+    public List<String> getList () {
+        List<String> newList = new ArrayList<>();
+        newList.add("Html5");
+        newList.add("css3");
+        newList.add("js");
+        newList.add("react");
+        newList.add("angular");
+        return newList;
+    }
 }
